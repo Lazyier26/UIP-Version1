75879e1b189cee39bef181281034aad8890c15a4
ladwty <162706033+ladwty@users.noreply.github.com>
2025-08-19 14:11:35 +0900

    --- update navbar

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+/*HAMBURGER MENU*/
+let menu = document.querySelector('#nav-toggle');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('active');
+  navbar.classList.toggle('open');
+  document.body.classList.toggle('sidebar-open');
+};
+
