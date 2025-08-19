94f76d8892574620cbfa917f1270994db1f3a4f4
ladwty <162706033+ladwty@users.noreply.github.com>
2025-08-19 14:11:35 +0900

    --- update navbar

--- Assets/js/nav-script.js
+++
@@ -1,8 +0,0 @@
-/*SHOW MENU*/
-let menu = document.querySelector('#nav-toggle');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-  menu.classList.toggle('active');  // for icon switch
-  navbar.classList.toggle('open');  // for showing menu
-};
