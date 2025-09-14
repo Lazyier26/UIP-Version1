7080df48fbc2d069fc712534e69fe9047b318ddb
Jason Inosante <jasoninosante26@gmail.com>
2025-09-15 07:37:23 +0800

    config php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // Database configuration
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root'); // Change this to your MySQL username
-define('DB_PASS', ''); // Change this to your MySQL password
+define('DB_USER', 'UIPdatabase'); // Change this to your MySQL username
+define('DB_PASS', 'UIPincorporated2021'); // Change this to your MySQL password
 define('DB_NAME', 'uip_registration');
 
 // File upload configuration
